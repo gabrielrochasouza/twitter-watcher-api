2efd87ca9df31c3ae94d7d9d252b52ea3abe69c3
Gabriel Rocha <gabrielrs7@outlook.com>
2022-08-01 01:30:58 -0300

    feat: adding gitignore and user not found

--- routes/api.route.js
+++ routes/api.route.js
@@ -3,40 +3,49 @@ const router = require("express").Router();
 const base_url = "https://api.twitter.com/";
 const token = `Bearer ${process.env.BEARER_TOKEN}`;
 
-const Twitter = require('twitter')
- 
+const Twitter = require("twitter");
+
 const client = new Twitter({
-  consumer_key:         'QTZvBsqyQsjssq2BjD4dFUDDj',
-  consumer_secret:      'PH8sFhaiFvftgbS3q5UnKBfoiRw1R4diJeS4WRDFaL8MBpXK34',
-  access_token_key:         '1548827477694418951-ucDQghp6Ehnwq0KeMDqdJA6EXQ7mc6',
-  access_token_secret:  'LUApSD18bCuM3B3dA1Yy0HNlv29qauJjNxiNA5zkhq770',
-})
-
-
-router.get("/user", async (req, res, next) => {
-  const username = req.query.username
-  const url = `2/users/by?usernames=${username}&user.fields=description%2Cprofile_image_url`;
-
-  const id = req.query.woeid
-  const user = await client.get(`${base_url}${url}`,{
-
-  }).catch(err=>console.log(err))
-  
-  res.send(user);
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
+router.get("/user", async (req, res, next) => {
+  const username = req.query.username;
+  const url = `2/users/by?usernames=${username}&user.fields=description%2Cprofile_image_url`;
+
+  const id = req.query.woeid;
+  try {
+    const user = await client
+      .get(`${base_url}${url}`, {})
+      .catch((err) => console.log(err));
+    if (!user) {
+      res.status(404).json({ message: "user not found" });
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(404).json({ message: "user not found" });
+  }
+});
 
 router.get("/twittes", async (req, res, next) => {
-  const userId = req.query.userid
+  const userId = req.query.userid;
 
   const url = `2/users/${userId}/tweets`;
- 
-  const id = req.query.woeid
-  const twittes = await client.get(`${base_url}${url}`,{
 
-  }).catch(err=>console.log(err))
-  
-  res.send(twittes);
+  try {
+    const twittes = await client
+      .get(`${base_url}${url}`, {})
+      .catch((err) => console.log(err));
+    if (!twittes) {
+      res.status(404).json({ message: "user not found" });
+    }
+    res.send(twittes);
+  } catch (err) {
+    res.status(404).json({ message: "user not found" });
+  }
 });
 
 module.exports = router;
